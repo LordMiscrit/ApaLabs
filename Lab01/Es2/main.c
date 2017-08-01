63b6c48ec092fd507832c2ff7714e843ad6852f3
LordMiscrit <lord.miscrit@gmail.com>
2017-08-01 18:12:43 +0200

    lab1 es1

--- Lab01/Es2/main.c
+++ Lab01/Es2/main.c
@@ -1,7 +1,45 @@
 //
 // Created by Michel on 31/07/2017.
 //
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 void main(){
+
+    int fine=0;
+    float prev=0;
+    char operatore,op1s[16],op2s[16];
+    while(!fine){
+        scanf("%c",&operatore);
+        if(operatore!='+' && operatore!='-' && operatore!='*' && operatore!='/'){
+            printf("\nfine\n");
+            fine=1;
+        }else{
+            scanf("%s %s",op1s,op2s);
+            fflush();
+            switch (operatore){
+                case '+':
+                    prev= operando(op1s,prev) + operando(op2s,prev);
+                    break;
+                case '-':
+                    prev= operando(op1s,prev) - operando(op2s,prev);
+                    break;
+                case '*':
+                    prev= operando(op1s,prev) * operando(op2s,prev);
+                    break;
+                case '/':
+                    prev= operando(op1s,prev) / operando(op2s,prev);
+                    break;
+                default:break;
+            }
+        }
+    }
+
     return;
+}
+
+float operando(char* op,float prev){
+    if(!strcmp(op,"PREV"))return prev;
+    return (flaot) atof(op);
 }
